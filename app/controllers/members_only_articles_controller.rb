44809e0cc989804d934dd8e43f499b9277a98c35
Samueelx <gsammygitau@gmail.com>
2023-03-30 21:09:44 +0300

    done.

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,4 +1,5 @@
 class MembersOnlyArticlesController < ApplicationController
+  before_action :authorize
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
@@ -14,7 +15,11 @@ class MembersOnlyArticlesController < ApplicationController
   private
 
   def record_not_found
-    render json: { error: "Article not found" }, status: :not_found
+    render json: { error: "Article not found" }, status: :not_found 
+  end
+
+  def authorize
+    render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :user_id
   end
 
 end
